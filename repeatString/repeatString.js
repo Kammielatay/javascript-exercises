ab561e8bf7a94ccfb0cd4be769f27329c851c618
Kameshia Ingram <kammie@Kameshias-Air.domain>
2020-10-13 18:00:44 -0700

    finished repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(str,times) {
+    let results = '';
+    while(times > 0) {
+        results +=str;
+        times--;
+    } return results;
 
-}
+} 
 
 module.exports = repeatString
